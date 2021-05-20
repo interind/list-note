e357d180025709ce1a041b789bbd467a97e9498d
Aleksandr <interind@yandex.ru>
2021-05-20 17:16:35 +0300

    create and delete task

--- app.js
+++ app.js
@@ -37,7 +37,17 @@ const tasks = [
     return acc;
   }, {});
 
+  // Elements UI
+
+  const listContainer = document.querySelector('.tasks-list-section .list-group');
+  const form = document.forms['addTask'];
+  const {title: inputTitle, body: inputBody} = form.elements;
+
   renderOfTasks(objOfTasks);
+
+  // events
+  form.addEventListener('submit', onFormSubmitHandler);
+  listContainer.addEventListener('mouseup', onDeleteHandler);
 
   function renderOfTasks(tasksList) {
     if(!tasksList) return alert('Передайте список задач');
@@ -45,7 +55,9 @@ const tasks = [
 
     Object.values(tasksList).forEach((task) => {
       const li = listItemTemplate(task);
+      fragment.appendChild(li);
     });
+    listContainer.appendChild(fragment);
   }
 
   function listItemTemplate({_id, title, body}) {
@@ -57,5 +69,76 @@ const tasks = [
       'flex-wrap',
       'mt-2',
     );
+    li.setAttribute('data-task-id', _id);
+
+    const span = document.createElement('span');
+    span.textContent = title;
+    span.style.fontWeight = 'bold';
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'delete_task';
+    deleteButton.classList.add(
+      'btn',
+      'btn-danger',
+      'ml-auto',
+      'delete-btn',
+    );
+
+    const article = document.createElement('p');
+    article.textContent = body;
+    article.classList.add(
+      'mt-2',
+      'w-100',
+    );
+
+    li.appendChild(span);
+    li.appendChild(deleteButton);
+    li.appendChild(article);
+    return li;
+  }
+
+  function onFormSubmitHandler(evt) {
+    evt.preventDefault();
+    if (!inputTitle.value || !inputBody.value) {
+      alert('Введите данные для заметки!')
+      return;
+    }
+    const task = createNewTask(inputTitle.value, inputBody.value);
+    const listItem = listItemTemplate(task);
+    listContainer.insertAdjacentElement('afterbegin', listItem);
+    form.reset();
+  }
+  function createNewTask(title, body) {
+    const newTask = {
+      title,
+      body,
+      complited: false,
+      _id: `task-${Math.random()}`,
+    };
+    objOfTasks[newTask._id] = newTask; // добавление объекта в объект задач
+
+    return { ...newTask };
+  }
+
+  function deleteTask(id) {
+    const isConfirm = id ? confirm(`Удалить задачу: ${objOfTasks[id].title} ???`) : false;
+    if (!isConfirm) return isConfirm;
+    delete objOfTasks[id];
+    return isConfirm;
+  }
+
+  function deleteTaskForHtml(isDelete, element) {
+    if (!isDelete) return;
+      element.remove();
+      element = null;
+  }
+
+  function onDeleteHandler({ target }) {
+    if (target.classList.contains('delete-btn')) {
+      const parent = target.closest('[data-task-id]');
+      const id = parent.dataset.taskId;
+      const confirmed = deleteTask(id);
+      deleteTaskForHtml(confirmed, parent);
+    }
   }
 })(tasks);

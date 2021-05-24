8deaa9eeed54e4ec793e4921a2228d61823dfa3e
Aleksandr <interind@yandex.ru>
2021-05-24 22:08:51 +0300

    fetch

--- pages/app.js
+++ pages/app.js
@@ -12,25 +12,25 @@ function closePopup() {
 
 const tasks = [
   {
-    _id: '5d2ca9e2e03d40b326596aa7',
+    _id: '03d40b3',
     completed: true,
     body:
       'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
   },
   {
-    _id: '5d2ca9e29c8a94095c1288e0',
+    _id: '94095c1288e0',
     completed: false,
     body:
       'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
   },
   {
-    _id: '5d2ca9e2e03d40b3232496aa7',
+    _id: '532496aa7',
     completed: true,
     body:
       'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
   },
   {
-    _id: '5d2ca9e29c8a94095564788e0',
+    _id: '4788e0',
     completed: false,
     body:
       'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
@@ -43,82 +43,12 @@ const tasks = [
     return acc;
   }, {});
 
-  const themes = {
-    default: {
-      '--base-text-color': '#212529',
-      '--header-bg': '#007bff',
-      '--header-text-color': '#fff',
-      '--default-btn-bg': '#007bff',
-      '--default-btn-text-color': '#fff',
-      '--default-btn-hover-bg': '#0069d9',
-      '--default-btn-border-color': '#0069d9',
-      '--danger-btn-bg': '#dc3545',
-      '--danger-btn-text-color': '#fff',
-      '--danger-btn-hover-bg': '#bd2130',
-      '--danger-btn-border-color': '#dc3545',
-      '--input-border-color': '#ced4da',
-      '--input-bg-color': '#fff',
-      '--input-text-color': '#495057',
-      '--input-focus-bg-color': '#fff',
-      '--input-focus-text-color': '#495057',
-      '--input-focus-border-color': '#80bdff',
-      '--input-focus-box-shadow': '0 0 0 0.2rem rgba(0, 123, 255, 0.25)',
-    },
-    dark: {
-      '--base-text-color': '#212529',
-      '--header-bg': '#343a40',
-      '--header-text-color': '#fff',
-      '--default-btn-bg': '#58616b',
-      '--default-btn-text-color': '#fff',
-      '--default-btn-hover-bg': '#292d31',
-      '--default-btn-border-color': '#343a40',
-      '--default-btn-focus-box-shadow':
-        '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
-      '--danger-btn-bg': '#b52d3a',
-      '--danger-btn-text-color': '#fff',
-      '--danger-btn-hover-bg': '#88222c',
-      '--danger-btn-border-color': '#88222c',
-      '--input-border-color': '#ced4da',
-      '--input-bg-color': '#fff',
-      '--input-text-color': '#495057',
-      '--input-focus-bg-color': '#fff',
-      '--input-focus-text-color': '#495057',
-      '--input-focus-border-color': '#78818a',
-      '--input-focus-box-shadow': '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
-    },
-    light: {
-      '--base-text-color': '#212529',
-      '--header-bg': '#fff',
-      '--header-text-color': '#212529',
-      '--default-btn-bg': '#fff',
-      '--default-btn-text-color': '#212529',
-      '--default-btn-hover-bg': '#e8e7e7',
-      '--default-btn-border-color': '#343a40',
-      '--default-btn-focus-box-shadow':
-        '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
-      '--danger-btn-bg': '#f1b5bb',
-      '--danger-btn-text-color': '#212529',
-      '--danger-btn-hover-bg': '#ef808a',
-      '--danger-btn-border-color': '#e2818a',
-      '--input-border-color': '#ced4da',
-      '--input-bg-color': '#fff',
-      '--input-text-color': '#495057',
-      '--input-focus-bg-color': '#fff',
-      '--input-focus-text-color': '#495057',
-      '--input-focus-border-color': '#78818a',
-      '--input-focus-box-shadow': '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
-    },
-  };
-  // let lastDefaultTheme = 'default';
-
-  // Elements UI
-
   const listContainer = document.querySelector('.container-list');
+  const convertContainer = document.querySelector('.container-list-req');
   const form = document.forms['add-task'];
   const { body: inputBody } = form.elements;
 
   renderOfTasks(objOfTasks);
-  console.log(form);
 
   // Events
   form.addEventListener('submit', onFormSubmitHandler);
@@ -139,19 +69,21 @@ window.addEventListener('keyup', (evt) => {
   function renderOfTasks(tasksList) {
     if(!tasksList) return alert('Напишите сообщение');
     const fragment = document.createDocumentFragment();
+    const fragmentRequest = document.createDocumentFragment();
 
     Object.values(tasksList).forEach((task) => {
-      const li = listItemTemplate(task);
-      fragment.appendChild(li);
+      fragment.appendChild(listItemTemplate(task, true));
+      fragmentRequest.appendChild(listItemTemplate(task, false));
     });
     listContainer.appendChild(fragment);
+    convertContainer.appendChild(fragmentRequest);
   }
 
-  function listItemTemplate({_id, body}) {
+  function listItemTemplate({_id, body}, bool) {
     const li = document.createElement('li');
     li.classList.add('container-list__item');
     li.setAttribute('data-task-id', _id);
-    li.textContent = body;
+    li.textContent = bool ? `#${_id} ${body}` : `Длинна строки #${_id}: ${body.length}`;
     return li;
   }
 
@@ -161,36 +93,18 @@ window.addEventListener('keyup', (evt) => {
       return;
     }
     const task = createNewTask(inputBody.value);
-    const listItem = listItemTemplate(task);
-    listContainer.insertAdjacentElement('afterbegin', listItem);
+    listContainer.insertAdjacentElement('afterbegin', listItemTemplate(task, true));
+    convertContainer.insertAdjacentElement('afterbegin', listItemTemplate(task, false));
     form.reset();
   }
   function createNewTask(body) {
     const newTask = {
       body,
       complited: false,
-      _id: `task-${Math.random()}`,
+      _id: `-${Math.random()}`,
     };
     objOfTasks[newTask._id] = newTask; // добавление объекта в объект задач
 
     return { ...newTask };
-  }
-
-  // function onThemeSelectHandler() {
-  //   const selectedTheme = themeSelect.value;
-  //   const isConfirm = confirm(`Изменить тему: ${selectedTheme.toUpperCase()}`);
-  //   if(!isConfirm) {
-  //     themeSelect.value = lastDefaultTheme;
-  //     return;
-  //   }
-
-  //   setTheme(selectedTheme);
-  //   lastDefaultTheme = selectedTheme;
-
-  function setTheme(name) {
-    const selectedThemeObj = themes[name];
-    Object.entries(selectedThemeObj).forEach(([key, value]) => {
-      document.documentElement.style.setProperty(key, value);
-    })
   }
 })(tasks);
